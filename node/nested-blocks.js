ddec69a559fe7c5de0ea7cb87f47fd0ecf0208a5
Blaine Price <1wbprice@gmail.com>
2018-03-14 00:06:01 -0400

    write simpler logic for finding pairs

--- node/nested-blocks.js
+++ node/nested-blocks.js
@@ -20,71 +20,35 @@ const body = `
 {{/if}}
 `;
 
-function isOpen(helper) {
-    return helper.indexOf('{{#') !== -1;
-}
-
-function isClose(helper) {
-    return helper.indexOf('{{/') !== -1;
-}
-
-function findHelper(body, index) {
-    const match = helper_rgx.exec(body);
-    return match && {
-      text: match[0],
-      type: match[1] + match[2].split(' ')[0],
-      subtype: match[2].split(' ')[0],
-      _index: match.index
+function findHelper(body, from) {
+    if (!from) {
+        from = 0;
     }
+    const helper = body.slice(from).match(helper_rgx);
+    return helper && {
+        text: helper[0],
+        type: helper[1] === '#' ? 'open' : 'close',
+        expression: helper[2].split(' ').shift(),
+        index: helper.index + from,
+        end: helper.index + from + helper[0].length
+    };
 }
-
-function fixHelperIndex(helpers) {
-    let index = 0;
-    return helpers.map(helper => {
-        const hlp = Object.assign({}, {
-            text: helper.text,
-            type: helper.type,
-            subtype: helper.subtype,
-            index: index
-        });
-        index += hlp.length;
-        return hlp;
-    });
-};
 
 function findHelpers(body, helpers) {
     if (!helpers) {
-      helpers = [];
+        helpers = [];
     }
-    const helper = findHelper(body);
-    
+
+    const lastHelper = helpers[helpers.length - 1];
+    const from = lastHelper && lastHelper.end;
+    const helper = findHelper(body, from)
+
     if (!helper) {
-      return helpers;
+        return helpers;
     }
 
-    helpers.push(helper)
-    const startIndex = helper._index + helper.text.length;
-    return findHelpers(body.slice(startIndex), helpers);
+    helpers.push(helper);
+    return findHelpers(body, helpers);
 }
 
-function pairHelpers(helpers, pairs) {
-    if (!pairs) {
-      pairs = [];
-    }
-
-    helpers.forEach((helper, index, helpers) => {
-        // find this helper's pair
-      helpers.slice(1).some((hlp) => {
-        if (helper.subtype === hlp.subtype) {
-            if (isOpen(helper.text) && isClose(hlp.text)) {
-                pairs.push({
-                    open: helper,
-                    close: hlp
-                });
-            }
-        } 
-      });
-    });
-
-  return pairs;
-}
+console.log(findHelpers(body));
